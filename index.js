3a2ef6bb6376314db60a6b4340ff09a5928599ac
roky18 <rokymax66@gmail.com>
2025-12-09 16:45:12 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,62 @@
+const express = require("express");
+const cors = require("cors");
+const app = express();
+require("dotenv").config();
+
+const { MongoClient, ServerApiVersion } = require("mongodb");
+
+const port = process.env.PORT || 3000;
+
+// MiddleWare--->
+app.use(express.json());
+app.use(cors());
+// MiddleWare---<
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.wxnpgmj.mongodb.net/?appName=Cluster0`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+
+    const db = client.db("digital_life_lessons_db");
+    const lessonCollection = db.collection("lesson");
+
+    // Lesson Related API---->>>
+    app.get("/lessons", async (req, res) => {});
+
+    app.post("/lessons", async (req, res) => {
+      const lesson = req.body;
+      const result = await lessonCollection.insertOne(lesson);
+    });
+
+    // Lesson Related API----<<<
+
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+app.get("/", (req, res) => {
+  res.send("Digital Life Lessoning........!");
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
